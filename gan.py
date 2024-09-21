f1ccb46035d22966643b5872e29e48c257817b66
yk9872 <yk9872@exo.att.com>
2024-09-21 18:49:48 +0800

    added 1 more feature in lstm in gan

--- gan.py
+++ gan.py
@@ -9,39 +9,46 @@ import datetime
 def parser(x):
     return datetime.datetime.strptime(x, '%Y-%m-%d')
 
-dataset = pd.read_csv('data/panel_data_close.csv', header=0, parse_dates=[0], date_parser=parser)
-num_training_days = int(dataset.shape[0]*.7)
+# Load dataset
+dataset = pd.read_csv('data/output.csv', header=0)
+
+# Define number of training days
+num_training_days = int(dataset.shape[0] * 0.7)
 
 train_data, test_data = dataset[:num_training_days], dataset[num_training_days:]
 
-time = np.array(dataset['Date'])
-data = np.array(dataset['price'])
+# Extract multiple features, assuming 'price' and 'MACD' are columns in the dataset
+data = dataset[['price', 'MACD']].values  # Adjust based on your actual features
 
 def create_dataset(data, time_step=1):
     X, y = [], []
     for i in range(len(data) - time_step - 1):
         a = data[i:(i + time_step)]
         X.append(a)
-        y.append(data[i + time_step])
+        y.append(data[i + time_step, 0])  # Predicting the price (first column)
     return np.array(X), np.array(y)
 
 time_step = 10  # Number of time steps to look back
 X, y = create_dataset(data, time_step)
 
 # Reshape input to be [samples, time steps, features]
-X = X.reshape(X.shape[0], X.shape[1], 1)
+X = X.reshape(X.shape[0], X.shape[1], X.shape[2])  # Shape: (samples, time_step, features)
 
 model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(time_step, 1)))
+model.add(LSTM(50, return_sequences=True, input_shape=(time_step, X.shape[2])))  # Use number of features
 model.add(LSTM(50))
-model.add(Dense(1))
+model.add(Dense(1))  # Output layer for price prediction
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 
+# Fit the model
 model.fit(X, y, epochs=100, batch_size=32)
 
 # Starting point for the generation
-last_sequence = data[-time_step:].reshape(1, time_step, 1)
+last_sequence = data[-time_step:].reshape(1, time_step, data.shape[1])  # Shape: (1, time_step, num_features)
+
+# Before the generation loop
+last_known_volume = data[-1, 1]  # Get the last known volume from the original data
 
 generated_data = []
 num_generate = len(test_data)  # Number of new data points to generate
@@ -58,25 +65,26 @@ for _ in range(num_generate):
     # Append the predicted value to the generated data
     generated_data.append(next_value_scalar)
 
-    # Reshape next_value_scalar to (1, 1, 1)
-    next_value_reshaped = np.array(next_value_scalar).reshape(1, 1, 1)  # Shape: (1, 1, 1)
-    
-    print(f"next_value_reshaped shape: {next_value_reshaped.shape}")
+    # Prepare the next input sequence for the model
+    # Here, you need to create a new sequence by appending the new value.
+    next_value_reshaped = np.array([[next_value_scalar, last_sequence[0, -1, 1]]]).reshape(1, 1, data.shape[1])  # Replace the volume with the last known value or set a default
 
     # Update last_sequence by appending the new value
     last_sequence = np.append(last_sequence[:, 1:, :], next_value_reshaped, axis=1)
     
     print(f"Updated last_sequence shape: {last_sequence.shape}")
 
+# Create an array for generated data with the same number of features
+generated_data_full = np.array([[val, last_known_volume] for val in generated_data])
 # Combine original and generated data for visualization
-full_data = np.concatenate((data, generated_data))
+full_data = np.concatenate((data, generated_data_full), axis=0)
+
 
 # Visualize the results
-plt.plot(np.arange(len(data)), data, label='Original Data')
-plt.plot(np.arange(len(data), len(full_data)), generated_data, label='Generated Data', color='red')
+plt.plot(np.arange(len(data)), data[:, 0], label='Original Price Data')  # Plot original price
+plt.plot(np.arange(len(data), len(full_data)), generated_data, label='Generated Price Data', color='red')
 plt.title("Generated Time Series Data")
 plt.xlabel("Time")
-plt.ylabel("Value")
+plt.ylabel("Price")
 plt.legend()
 plt.show()
-
--- test.py
+++ test.py
@@ -2,22 +2,17 @@ import numpy as np
 
 # Assume VAE_data is your input data
 # If VAE_data is a pandas DataFrame, convert it to a numpy array
-VAE_data = np.random.rand(100, 1).astype(np.float32)  # Example data with 1000 samples
+
+VAE_data = np.random.rand(10, 3).astype(np.float32)  # Example data with 1000 samples
 batch_size = 5
 n_output = VAE_data.shape[1] - 1  # Adjust if necessary
 
 print('----- data ---- {}'.format(VAE_data))
 
-# Create a batch of data
-# train = VAE_data[:batch_size,1:]  # Get the first 'batch_size' samples
-# real = VAE_data[batch_size:,:1]
-# print('----- train ---- {}'.format(train))
-# print('----- real ---- {}'.format(real))
+print('----- VAE_data.shape ---- {}'.format(VAE_data.shape))
 
-arr = [1,2,3,4,5,6,7]
-i = 0
-test_step = 2
-print('----- test ---- {}'.format(arr[i + 2]))
-print('----- test ---- {}'.format(arr[0]))
-print('----- test ---- {}'.format(arr[1]))
-print('----- test ---- {}'.format(arr[6]))
+VAE_data = VAE_data.reshape(VAE_data.shape[0], VAE_data.shape[1], 1)
+
+print('----- after VAE_data.shape ---- {}'.format(VAE_data.shape))
+
+print('----- data after reshape---- {}'.format(VAE_data))
