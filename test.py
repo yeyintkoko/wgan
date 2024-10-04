d7033417f6dd8af7298ad4a3f43479dbdb27325b
yk9872 <yk9872@exo.att.com>
2024-10-04 22:47:31 +0800

    best of all as of now

--- test.py
+++ test.py
@@ -2,18 +2,5 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-# np.set_printoptions(suppress=True, precision=6)
-
-scaler_y = StandardScaler()
-
-change_rate = scaler_y.fit_transform(np.array('6.8336185e-08').reshape(-1,1)).flatten()
-print(change_rate)
-
-change_rate = scaler_y.transform(np.array('-1.3667237e-07').reshape(-1,1)).flatten()
-print(change_rate)
-
-change_rate = scaler_y.transform(np.array('1.3667238e-07').reshape(-1,1)).flatten()
-print(change_rate)
-
-change_rate = scaler_y.transform(np.array('-0.0').reshape(-1,1)).flatten()
-print(change_rate)
+noise = np.random.normal(0, 29, (1, 50, 8))
+print(noise)
